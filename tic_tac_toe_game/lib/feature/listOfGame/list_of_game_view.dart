a780dede5880cc22915da1891ba1b7aed69047b1
Osmancan Akagündüz <83430594+osmancan23@users.noreply.github.com>
2024-08-14 12:40:10 +0300

    added stream builder config

--- tic_tac_toe_game/lib/feature/listOfGame/list_of_game_view.dart
+++ tic_tac_toe_game/lib/feature/listOfGame/list_of_game_view.dart
@@ -1,27 +1,17 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:tic_tac_toe_game/core/constants/enums/asssets_enum.dart';
 import 'package:tic_tac_toe_game/core/extensions/int_extensions.dart';
 import 'package:tic_tac_toe_game/core/extensions/num_extensions.dart';
 import 'package:tic_tac_toe_game/core/init/navigation/navigation_service.dart';
-import 'package:tic_tac_toe_game/core/service/supabase_service.dart';
 import 'package:tic_tac_toe_game/feature/game/game_view.dart';
 import 'package:tic_tac_toe_game/feature/gameCreate/game_create_view.dart';
 
 import '../../core/model/game_model.dart';
 part 'widget/game_item_widget.dart';
 
-class ListOfGameView extends StatefulWidget {
+class ListOfGameView extends StatelessWidget {
   const ListOfGameView({super.key});
-
-  @override
-  State<ListOfGameView> createState() => _ListOfGameViewState();
-}
-
-class _ListOfGameViewState extends State<ListOfGameView> {
-  bool isLoading = false;
-  List<GameModel> games = [];
 
   @override
   Widget build(BuildContext context) {
@@ -29,6 +19,7 @@ class _ListOfGameViewState extends State<ListOfGameView> {
         body: Center(
       child: SingleChildScrollView(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             100.ph,
             ElevatedButton(
@@ -38,29 +29,61 @@ class _ListOfGameViewState extends State<ListOfGameView> {
               ),
               child: const Text("Create Game"),
             ),
-            FutureBuilder(
-              future: SupabaseDbService.instance.getGames(),
-              builder: (BuildContext context, AsyncSnapshot snapshot) {
-                if (snapshot.hasData) {
-                  return ListView.builder(
-                    itemCount: snapshot.data.length,
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemBuilder: (BuildContext context, int index) {
-                      GameModel game = snapshot.data[index];
-                      return _GameItemWidget(gameModel: game);
-                    },
-                  );
-                } else if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
-                } else {
-                  return const CircularProgressIndicator();
-                }
-              },
-            ),
+            const _GameStreamListWidget(),
           ],
         ),
       ),
     ));
+  }
+}
+
+class _GameStreamListWidget extends StatefulWidget {
+  const _GameStreamListWidget();
+
+  @override
+  State<_GameStreamListWidget> createState() => __GameStreamListWidgetState();
+}
+
+class __GameStreamListWidgetState extends State<_GameStreamListWidget> {
+  List<GameModel> games = [];
+  late Stream gameStream;
+  @override
+  void initState() {
+    super.initState();
+    gameStream = Supabase.instance.client.from("games").stream(primaryKey: ["id"]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: gameStream,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          if (snapshot.hasData) {
+            _handleData(snapshot);
+
+            return ListView.builder(
+              itemCount: games.length,
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemBuilder: (BuildContext context, int index) {
+                GameModel game = games[index];
+                return _GameItemWidget(gameModel: game);
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            return const CircularProgressIndicator();
+          }
+        } else {
+          return const CircularProgressIndicator();
+        }
+      },
+    );
+  }
+
+  void _handleData(AsyncSnapshot<dynamic> snapshot) {
+    games = snapshot.data.map((e) => GameModel.fromJson(e)).toList().cast<GameModel>();
   }
 }
